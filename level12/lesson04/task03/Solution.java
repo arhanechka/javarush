15dec84b8ad30f2c6755de031bf99e2f48641240
arhanechka <arhanechka@gmail.com>
2017-01-31 01:05:48 +0200

    Add files via upload

---
+++ level12/lesson04/task03/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public void print (int a){}
+    public void print (int a, int b){}
+    public void print (String str){}
+    public void print (double c){}
+    public void print (char e){}
+}
