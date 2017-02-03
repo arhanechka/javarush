b6b8cfa641a7f5574ab8c2a386a3b24d2444f45c
arhanechka <arhanechka@gmail.com>
2017-02-03 12:05:21 +0200

    Solving of searching tag task with Stack

--- level19/lesson10/bonus03/Solution.java
+++ level19/lesson10/bonus03/Solution.java
@@ -27,10 +27,7 @@ text1, text2 могут быть пустыми
 */
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,61 +39,82 @@ public class Solution
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String fileName = br.readLine();
         File file = new File(fileName);
-        String openTag = "<"+args[0];
+        String openTag = "<" + args[0];
 
-        String closeTag = "</" + args[0]+">";
+        String closeTag = "</" + args[0] + ">";
 
         FileInputStream fis = new FileInputStream(file);
-        byte [] buff = new byte[(int) file.length()];
+        byte[] buff = new byte[(int) file.length()];
         fis.read(buff);
+        fis.close();
         String result = new String(buff);
-    result=result.replaceAll("\\r\\n", "");
-        result=result.substring(result.indexOf(openTag),result.lastIndexOf(closeTag))+closeTag;
-       // System.out.println(result);
-        int begin = result.indexOf(openTag);
-        int end = result.indexOf(closeTag);
-        String temp = result.substring(begin,end)+closeTag;
-        System.out.println(temp);
-Pattern pattern=Pattern.compile("[openTag]{2}");
-        Matcher matcher=pattern.matcher(temp.substring(openTag.length()));
-        if (matcher.find())
-{
-    String inner = temp.substring(temp.lastIndexOf(openTag));
-    System.out.println(temp);
-}
+        result = result.replaceAll("\\r\\n", "");
+        try
+        {
+            result = result.substring(result.indexOf(openTag), result.lastIndexOf(closeTag)) + closeTag;
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("There is no such tag in the document");
+        }
 
-     //   System.out.println(result);
-//
-//        String[] list1 = result.split(openTag);
-//         List<String> list = Arrays.asList(list1);
-//        for (String s : list)
-//        {if (!Pattern.matches (openTag, openTag))
-//        { if (s.startsWith(">")|| (s.startsWith(" ")))
-//           {if (!s.endsWith(closeTag))
-//           {int temp=s.indexOf(closeTag);
-//               System.out.println(openTag+s.substring(0,temp)+closeTag);}
-//            else
-//                System.out.println(openTag+s+closeTag);
-//            }}
-//            else
-//
-//        }
-//        for (String s : list)
-//        {
-//            System.out.println(s);
-//        }
-//        System.out.println();
-//        int i=0;
-//        while (i<list.size())
-//        {
-//            if (list.get(i).startsWith(openTag))
-//            for (int j=i; !list.get(j).endsWith(closeTag); j++)
-//            { System.out.println(list.get(j-1) +" ");
-//            i++;}
-//            else
-//                i++;
-//
-//        }
+        Stack<String> stack = new Stack<>();
+      int i = result.indexOf(args[0]);
+        int temp = result.indexOf(args[0], i + 1);
+        while (i < result.length())
+        {
+            temp = result.indexOf(args[0], i + 1);
+
+            if ((temp == -1) || (i == -1))
+            {
+                break;
+            } else if (result.charAt(i - 1) == '<' && result.charAt(temp - 1) == '/')
+            {
+                String substr = result.substring(i - 1, temp + args[0].length() + 1);
+                System.out.println(substr);
+                i = result.indexOf(args[0], temp + args[0].length() + 1);
+            } else if (result.charAt(i - 1) == '<' && result.charAt(temp - 1) == '<')
+            {
+                int endTag = result.indexOf(closeTag,i);
+                int first = result.indexOf(closeTag,i);
+                int beg = result.indexOf(closeTag,i);
+                int last = result.indexOf(closeTag,i);
+
+                while (beg > i)
+                {
+                    last = result.indexOf(closeTag, endTag) + args[0].length() + 3;
+                    String check = result.substring(0, first);
+                    beg = check.lastIndexOf(args[0]);
+                    String str = result.substring(beg - 1, last);
+                    stack.push(str);
+                    first = beg - 1;
+                    endTag = last;
+
+                }
+                while (!stack.empty())
+                {
+                    System.out.println(stack.pop());
+                }
+                i = last + 1;
+            }
+            else if (result.charAt(i-1)=='<')
+            {
+            temp=result.indexOf(closeTag,i);
+                System.out.println(result.substring(i - 1, temp)+closeTag);
+           i = result.indexOf(args[0], temp + args[0].length() +3);}
+            else
+            {i = result.indexOf(args[0], temp + args[0].length() +3);}
+        }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
