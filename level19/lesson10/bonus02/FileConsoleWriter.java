a7e76b05a411aeaebb51073fc250550fde65faa4
arhanechka <arhanechka@gmail.com>
2017-02-06 11:20:00 +0200

    new tasks

--- level19/lesson10/bonus02/FileConsoleWriter.java
+++ level19/lesson10/bonus02/FileConsoleWriter.java
@@ -6,77 +6,90 @@ package com.javarush.test.level19.lesson10.bonus02;
 При записи данных в файл, должен дублировать эти данные на консоль
 */
 
+import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileConsoleWriter extends FileWriter
 {
-    private FileWriter fcw;
-
     public FileConsoleWriter(String fileName) throws IOException
     {
         super(fileName);
-
     }
 
-
-    @Override
-    public String getEncoding()
+    public FileConsoleWriter(String fileName, boolean append) throws IOException
     {
-        return super.getEncoding();
+        super(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException
+    {
+        super(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException
+    {
+        super(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd)
+    {
+        super(fd);
     }
 
     @Override
     public void write(int c) throws IOException
     {
-        super.write(c);
-        System.out.println(c);
+        String num = "" + ((char) c);
+        write(num, 0, num.length());
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException
     {
-        super.write(cbuf, off, len);
+        String temp = "";
         for (char c : cbuf)
-        {
-            System.out.println(c);
-        }
+            temp += c;
+        write(temp, off, len);
     }
 
     @Override
     public void write(String str, int off, int len) throws IOException
     {
+        System.out.print(str.substring(off, off + len));
         super.write(str, off, len);
-        System.out.println(str);
     }
-
-    @Override
-    public void flush() throws IOException
-    {
-        super.flush();
-    }
-
-    @Override
-    public void close() throws IOException
-    {
-        super.close();
-    }
-
 
     @Override
     public void write(char[] cbuf) throws IOException
     {
-        fcw.write(cbuf);
-        for (char c : cbuf)
-        {
-            System.out.println(cbuf);
-        }
+        write(cbuf, 0, cbuf.length);
     }
 
     @Override
     public void write(String str) throws IOException
     {
-        fcw.write(str);
-        System.out.println(str);
+        write(str, 0, str.length());
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        String s = "Hello world";
+        //char[] chars = new char[]{'4', '6', 'a'};
+        //int number = 78;
+        //char[] chars2 = new char[]{'4', '6', 'a', 'y', 'w'};
+        //String str1 = "My first String";
+
+        File file = new File("./1.txt");
+        FileConsoleWriter fcw = new FileConsoleWriter(file);
+
+        fcw.write(s);
+        //fcw.write(chars);
+        //fcw.write(number);
+        //fcw.write(chars2, 1, 3);
+        //fcw.write(str1, 3, 5);
+
+        fcw.close();
     }
 }
